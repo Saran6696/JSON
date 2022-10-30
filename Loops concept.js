4cafeb572641ed15432c5db0aa3aecd98dda9190
Saran6696 <116982240+Saran6696@users.noreply.github.com>
2022-10-31 01:00:26 +0530

    initial commit

---
+++ Loops concept.js
@@ -0,0 +1,41 @@
+let array = [ 
+    {
+      "id": "1",
+      "name": "Lura Senger",
+      "email": "Xander_Collier@yahoo.com"
+    },
+    {
+      "id": "2",
+      "name": "Wilburn Weber",
+      "email": "Bennett_Kreiger11@yahoo.com"
+    },
+    {
+      "id": "3",
+      "name": "Tyrique Hahn",
+      "email": "Juston.Altenwerth@yahoo.com"
+    },
+    {
+      "id": "4",
+      "name": "Lenny Bailey",
+      "email": "Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+      "id": "5",
+      "name": "Vladimir Keeling",
+      "email": "Louisa_Walsh18@hotmail.com"
+    },
+ ]
+
+
+ for (let i = 0; i<array.length; i++){
+  console.log(array[i]["id"])
+ }
+
+for( let i of array) {
+    console.log(i['name']);   
+}
+
+for(let i in array){
+console.log(array[i]['email']);
+}
+
